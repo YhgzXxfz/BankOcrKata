2b1d2e85d03725b8921af9e0e83e39ea0d761693
Qingfei.You <qingfeiyou@gmail.com>
2016-09-10 14:37:29 +0200

    feat(bank): generate report and try to correct invalid numbers

--- Ocr/src/main/java/Bank.java
+++ Ocr/src/main/java/Bank.java
@@ -1,17 +1,33 @@
+import java.util.*;
+
 public class Bank {
-    public static final int RAW_ACCOUNT_NUMBER_LENGTH = Ocr.TOTAL_DIGITS_IN_ACCOUNT_NUMBER * Ocr.SINGLE_RAW_NUMBER_WIDTH * Ocr.SINGLE_RAW_NUMBER_HEIGHT;
+    private static final int RAW_ACCOUNT_NUMBER_LENGTH = Ocr.TOTAL_DIGITS_IN_ACCOUNT_NUMBER * Ocr.SINGLE_RAW_NUMBER_WIDTH * Ocr.SINGLE_RAW_NUMBER_HEIGHT;
     private final Account account;
     private final Ocr ocr;
-    private String rawAccountNumbers;
+    private final String originalRawAccountNumbers;
+    private Map<Character, List<Character>> mp = new HashMap<>();
 
-    public Bank(String rawAccountNumbers) {
-        this.rawAccountNumbers = rawAccountNumbers;
+    public Bank(String originalRawAccountNumbers) {
+        this.originalRawAccountNumbers = originalRawAccountNumbers;
         this.ocr = new Ocr();
         this.account = new Account();
+
+        mp.put('0', Arrays.asList('8'));
+        mp.put('1', Arrays.asList('7'));
+        mp.put('2', Arrays.asList('2'));
+        mp.put('3', Arrays.asList('3'));
+        mp.put('4', Arrays.asList('4'));
+        mp.put('5', Arrays.asList('6', '9'));
+        mp.put('6', Arrays.asList('5', '8'));
+        mp.put('7', Arrays.asList('1'));
+        mp.put('8', Arrays.asList('0', '6', '9'));
+        mp.put('9', Arrays.asList('5', '8'));
+
     }
 
     public String generateReport() {
-        int length = rawAccountNumbers.length() / RAW_ACCOUNT_NUMBER_LENGTH;
+        int length = originalRawAccountNumbers.length() / RAW_ACCOUNT_NUMBER_LENGTH;
+        String rawAccountNumbers = originalRawAccountNumbers;
         StringBuilder report = new StringBuilder();
         for (int i = 0; i < length; ++i) {
             generateReport(report, ocr.parseRawNumbers(rawAccountNumbers.substring(0, RAW_ACCOUNT_NUMBER_LENGTH)));
@@ -30,5 +46,54 @@ public class Bank {
         if (account.isIllegal()) return " ILL";
         if (!account.isValid()) return " ERR";
         return "";
+    }
+
+    public String generateReportWithCorrection() {
+        int length = originalRawAccountNumbers.length() / RAW_ACCOUNT_NUMBER_LENGTH;
+        StringBuilder report = new StringBuilder();
+        String rawAccountNumbers = originalRawAccountNumbers;
+
+        for (int j = 0; j < length; ++j) {
+            generateReportWithCorrection(report, rawAccountNumbers.substring(0, RAW_ACCOUNT_NUMBER_LENGTH));
+            rawAccountNumbers = rawAccountNumbers.substring(RAW_ACCOUNT_NUMBER_LENGTH);
+        }
+        report.deleteCharAt(report.length() - 1);
+        return report.toString();
+    }
+
+    private void generateReportWithCorrection(StringBuilder report, String rawAccountNumber) {
+        String accountNumber = ocr.parseRawNumbers(rawAccountNumber);
+        account.setAccountNumber(accountNumber);
+
+        if (account.isValid()) {
+            report.append(accountNumber);
+        } else {
+            List<String> corrections = guessPossibleCorrections(accountNumber);
+            if (corrections.isEmpty()) {
+                report.append(accountNumber).append(" ERR");
+            } else {
+                report.append(corrections.get(0));
+                if (corrections.size() > 1) report.append(" AMB");
+            }
+        }
+        report.append('\n');
+    }
+
+    private List<String> guessPossibleCorrections(String accountNumber) {
+        List<String> corrections = new ArrayList<>();
+        char[] chars = accountNumber.toCharArray();
+        for (int i = 0; i < Ocr.TOTAL_DIGITS_IN_ACCOUNT_NUMBER; ++i) {
+            final char temp = chars[i];
+            for (char digit : mp.get(temp)) {
+                chars[i] = digit;
+                String correctedAccountNumber = new String(chars);
+                account.setAccountNumber(correctedAccountNumber);
+                if (account.isValid()) {
+                    corrections.add(correctedAccountNumber);
+                }
+            }
+            chars[i] = temp;
+        }
+        return corrections;
     }
 }
--- Ocr/src/test/java/BankTest.java
+++ Ocr/src/test/java/BankTest.java
@@ -26,4 +26,27 @@ public class BankTest {
         // Then
         assertThat(report).isEqualTo("457508000\n664371495 ERR\n86110??36 ILL");
     }
+
+    @Test
+    public void should_generate_report_with_correction_when_raw_account_numbers_are_invalid() {
+
+        // Given
+        final String rawAccountNumbers = "" +
+                "    _  _  _  _  _  _     _ " +
+                "|_||_|| || ||_   |  |  ||_ " +
+                "  | _||_||_||_|  |  |  | _|" +
+                "    _  _  _  _  _  _  _  _ " +
+                "|_||_   ||_ | ||_|| || || |" +
+                "  | _|  | _||_||_||_||_||_|" +
+                " _  _     _  _        _  _ " +
+                "|_ |_ |_| _|  |  ||_||_||_ " +
+                "|_||_|  | _|  |  |  | _| _|";
+        Bank bank = new Bank(rawAccountNumbers);
+
+        // When
+        String report = bank.generateReportWithCorrection();
+
+        // Then
+        assertThat(report).isEqualTo("490867715 AMB\n457508000\n664371485");
+    }
 }
